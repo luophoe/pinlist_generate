f4d7bf5068fd16ba2a07cbbb7e50101dbd79378e
Phoebe Luo <z65luo@uwaterloo.ca>
2021-10-29 11:10:55 +0800

    Update pinlist_generate.py

--- pinlist_generate.py
+++ pinlist_generate.py
@@ -33,3 +33,60 @@ def listconcat(string, num):
         string = string + "_output"
     return string
 
+
+# Function 3. for pinlist, change port number to the correct format for writing file
+def fileportnum(num):
+    if len(num) == 1:
+        # if port number is a one digit int, add 0 to front to fit bit format
+        num = "0" + num
+    return num
+
+
+# Function 4. for pinlist, change port number to the port number to the correct format for reading Excel
+def excelportnum(num):
+    if len(num) == 2 and num[0] == "0":
+        # if port number is a one digit int written in two digit, remove 0 at front to fit format
+        num = num[1]
+        return num
+
+
+# Function 5. for interface, look for bit width and reformat function name, update the corresponding list
+def reformname(string, num):
+    # if the function name is with width
+    if string.find("[") >= 0 and string.find("]") >= 0:
+        # if it has width, delete the width
+        string = string[:string.find("[")]
+        # if it is marked as input, format as _in and add it into func_Width_list
+        if num == 0:
+            # check if the name already has _in
+            string = checkending(string, 0)
+            string = string + "_in"
+        # if it is marked as output, format as _out and add it into func_width_list
+        elif num == 1:
+            # check if the name already has _out
+            string = checkending(string, 1)
+            string = string + "_out"
+
+        # if the name is already in func_width_list do not add into the list
+        if not(string in func_width_list):
+            func_width_list.append(string)
+
+    # if the function name is not with width
+    else:
+        # if it is marked as input, format as _in and add it into func_norm_list
+        if num == 0:
+            # check if the ending already has _in
+            string = checkending(string, 0)
+            string = string + "_in"
+        # if it is marked as output, format as _out and add it into func_norm_list
+        elif num == 1:
+            # check if the ending already has _out
+            string = checkending(string, 1)
+            string = string + "_out"
+
+        # if the name is already in func_norm_list do not add into the list
+        if not (string in func_norm_list):
+            func_norm_list.append(string)
+
+
+# Function 6. for pinlist, look for bit width and reformat it
