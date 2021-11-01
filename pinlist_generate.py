4028d34a0b0357e7317f08bf121ae87f11dcb759
Phoebe Luo <z65luo@uwaterloo.ca>
2021-11-01 17:31:18 +0800

    Update pinlist_generate.py

--- pinlist_generate.py
+++ pinlist_generate.py
@@ -7,7 +7,7 @@
 import xlrd
 import re
 
-# --------------------------------Input Intended Pinlist Information for Generation--------------------------------
+# -----------------------------------Input Intended Pinlist Information for Generation----------------------------------
 # input path of the excel file
 excel_path = r"/home/luozx/work/pinlist_generate/SnowbirdT5_MWP2_Pinlist_V0.0.5.xls"
 # input path of the pinlist sv files
@@ -16,14 +16,14 @@ seq_sv_path = r"/home/luozx/work/pinlist_generate/seq"
 test_sv_path = r"/home/luozx/work/pinnlist_generate/test"
 #input path of the interface sv file
 sv_path = r"/home/luozx/work/interface_generate"
-# ------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
 # Function 1.turn string to lower case
 def tolowercase(string):
     string = string.lower()
     return string
 
-# Function 2. concat the function name with _input and _output
+# Function 2. fo pinlist, concat the function name with _input and _output
 def listconcat(string, num):
     if num == 0:
         # concat _input
@@ -90,3 +90,117 @@ def reformname(string, num):
 
 
 # Function 6. for pinlist, look for bit width and reformat it
+def getbitwidth(string, num):
+    if string.find("[") >= 0 and string.find("]") >= 0:
+        # get the bit width number
+        find_width = string[string.find("["):string.find("]")]
+        width = re.findall(r'\d+', find_width)[0]
+        # strip the width
+        string = string[:string.find("[")]
+
+        # if it is marked as input, format as _in concat width
+        if num == 0:
+            # check if the ending already has _in
+            string = checkending(string, 0)
+            string = string + "_in" + width
+            width_list.append(string)
+        # if it is marked as output, format as _out concat width
+        elif num == 1:
+            # check if the ending already has _out
+            string = checkending(string, 1)
+            string = string + "_out" + width
+            width_list.append(string)
+
+    return string
+
+
+# Function 7. for pinlist, check the last section of the module name to return the formatted name
+def checklastsec(string, num):
+    if string.find("_") >= 0:
+        list_sec = string.split("_")
+        # extract the width number from the last section
+        width_num = re.findall(r'\d+', list_sec[-1])[0]
+        # extract the function name without the width number
+        width_func = "_".join(list_sec[:-1]) + "_" + "".join(i for i in list_sec[-1] if not i.isdigit())
+        if num == 0:
+            # if it is marked as input
+            string = width_func + "[" + width_num + "]"
+        elif num == 1:
+            # if it is marked as output
+            string = width_func + "(" + width_num
+    return string
+
+
+# Function 8. for pinlist, check the last section of the module name to return the formatted name without the width
+def striplastsec(string):
+    if string.find("_") >= 0:
+        list_sec = string.split("_")
+        # extract the function name without the width number
+        width_func = "_".join(list_sec[:-1]) + "_" + "".join(i for i in list_sec[-1] if not i.isdigit())
+    return width_func
+
+# FUnction 9. for both pinlist and interface, check the last section o fthe name and if _in and _out appears, strip the _in or _out
+def checkending(string, num):
+    # check if the name already has _in as an ending
+    if num == 0:
+        if string.find("_in") == len(string) - 3:
+            string = string[:len(string) - 3]
+    # check if the name already has _out as an ending
+    elif num == 1:
+        if string.find("_out") == len(string) - 4:
+            string = string[:len(string) - 4]
+    return string
+
+
+# ----------------------------------Part 1. Function to extract Excel info for pinlist----------------------------------
+def getlist(num):
+
+
+# ---------------------------------------Part 2. Function to write to seq sv file---------------------------------------
+def writeseqfile(list1, num):
+
+
+# ---------------------------------------Part 3. Function to write to test sv file--------------------------------------
+def writetestfile(num):
+
+
+# ---------------------------------Part 4. Function to extract Excel info for interface---------------------------------
+def getinterfacelist(num):
+
+
+# ------------------------------------Part 5. Function to write to interface sv file------------------------------------
+def writesvfile():
+
+
+# --------------------------------------Part 6. Global strings to write to sv file--------------------------------------
+string_1 = ""
+string_2 = ""
+
+
+# -----------------------------------------------------Part 7. Main-----------------------------------------------------
+# function names for interface
+func_norm_list = []
+# function with bit width for interface
+func_width_list = []
+
+for i in range(1, 41):
+    num = str(i)
+    getinterfacelist(num)
+    port_num = fileportnum(num)
+    # input path of the seq sv file
+    seq_path = seq_sv_path + "/gpio_" + port_num + "_new_domain_seq.sv"
+    # input path of the test sv file
+    test_path = test_sv_path + "/gpio_" + port_num + "_new_domain_test.sv"
+    # function names
+    func_list = []
+    # function with width
+    width_list = []
+
+    func_list = getlist(port_num)
+    if func_list == 0:
+        continue
+    writeseqfile(func_list, port_num)
+    writetestfile(port_num)
+
+sv_path = sv_path + "/pin_mux_interface_new.sv"
+writesvfile()
